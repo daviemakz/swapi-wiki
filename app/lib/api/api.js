b17a9ca49788714d16c2802e6df31e242d35c644
David Makuni <d.makuni@live.co.uk>
2017-02-10 17:48:26 +0000

    implemented lru caching

--- app/lib/api/api.js
+++ app/lib/api/api.js
@@ -1,4 +1,12 @@
 var http = require("http");
+var lru = require("lru-cache")
+  , options = { max: 500
+              , length: function (n, key) { return n * 2 + key.length }
+              , dispose: function (key, n) { n.close() }
+              , maxAge: 1000 * 60 * 60 }
+  , cache = lru(options)
+  , otherCache = lru(50)
+
 
 // swapi api base url
 var baseUrl = 'http://swapi.co/api/';
@@ -25,28 +33,35 @@ function getObject(id, type, callback) {
 // make http api request
 function makeRequest(slug, callback) {
   // return empty object literal if no slug defined
-  if (!slug) {
-    callback({
-      error: 1
-    })
+  if (!slug) { callback({ error: 1 }) }
+  // check if its in cache and return that or get the request
+  if (cache.peek(slug)) {
+    callback(cache.get(slug));
+  } else {
+    // make http request
+    var request = http.get(baseUrl + slug, function (response) {
+      var buffer = "",
+        data, route;
+      response.on("data", function (chunk) {
+        buffer += chunk;
+      });
+      response.on("end", function (err) {
+        if (err) {
+          callback({
+            error: 2
+          })
+        } else {
+          var responseBody = JSON.parse(buffer);
+          // save in cache
+          cache.set(slug, responseBody);
+          // callback
+          callback(responseBody);
+        }
+      });
+    });
   }
-  // make http request
-  var request = http.get(baseUrl + slug, function (response) {
-    var buffer = "",
-      data, route;
-    response.on("data", function (chunk) {
-      buffer += chunk;
-    });
-    response.on("end", function (err) {
-      if (err) {
-        callback({
-          error: 2
-        })
-      } else {
-        callback(JSON.parse(buffer))
-      }
-    });
-  });
+
+
 }
 
 // exports
